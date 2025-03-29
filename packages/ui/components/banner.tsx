2ac22b078034c882af39b245b2165337a50c4185
Jacob Beaumont <jcbeaumont@umass.edu>
2025-03-29 16:24:01 -0400

    feat: added background banner video
    - replaced solid background with looping stock video

--- packages/ui/components/banner.tsx
+++ packages/ui/components/banner.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../css/banner.css"; 
 import logo from "../../../apps/native/assets/cs-426-logo-light.svg"
+import goodDeedVideo from '../../../apps/native/assets/gooddeed.mp4';
+
 
 
 interface BannerProps {
@@ -10,6 +12,10 @@ interface BannerProps {
 export const Banner: React.FC<BannerProps> = ({ tagline }) => {
     return (
         <div className="banner">
+            <video autoPlay loop muted className="banner-video">
+                <source src={goodDeedVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <img src={logo} alt="logo" />
             <h1 className="tagline">{tagline}</h1>
         </div>
--- packages/ui/global.d.ts
+++ packages/ui/global.d.ts
@@ -3,3 +3,9 @@ declare module '*.svg' {
     const content: string;
     export default content;
 }
+
+// allows for importing of mp4 files
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
