4bb16b7ba5da0a6e186924a0fd1eb321752eb3e7
Phong <ptrinhha@umass.edu>
2025-03-30 18:46:08 -0400

    fix:add-constant-media

--- apps/web/src/pages/landingPage.tsx
+++ apps/web/src/pages/landingPage.tsx
@@ -6,7 +6,7 @@ import "./css/landingPage.css";
 export default function LandingPage() {
 
     return (
-        <div className="landing-container">
+        <div className="landing-container ">
             <Banner tagline="Connecting Hearts, Changing Lives" />
             <div className="landing-content">
                 <div className="cards-container">
