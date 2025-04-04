6e9a212d4302624ed6e1f6f13ac3996b7f902d83
Phong <ptrinhha@umass.edu>
2025-04-04 17:07:58 -0400

    bug:fix-vite-dependency

---
+++ packages/ui-web/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+    },
+  },
+});
